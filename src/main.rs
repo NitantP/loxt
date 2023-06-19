9fffe2fd3d8596e5bccf7cad9c9bdf61a1e3dc76
Nitant Patel <patel.nitant.bca@gmail.com>
2023-06-19 16:51:58 -0400

    chapter 15

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+use std::env;
+
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 
+const STACK_SIZE: usize = 256;
+
 type Value = f64;
+
+#[macro_export]
+macro_rules! binary_op {
+    ($self: expr, $op: tt) => {{
+        let left = $self.pop_stack();
+        let right = $self.pop_stack();
+        $self.push_stack(left $op right);
+    }};
+}
 
 #[derive(IntoPrimitive, TryFromPrimitive)]
 #[repr(u8)]
 enum OpCode {
     OpConstant,
+    OpAdd,
+    OpSubtract,
+    OpMultiply,
+    OpDivide,
+    OpNegate,
     OpReturn,
 }
 
@@ -33,6 +51,104 @@ impl Chunk {
     fn add_constant(&mut self, constant: Value) -> usize {
         self.constants.push(constant);
         return self.constants.len() - 1;
+    }
+}
+
+#[derive(IntoPrimitive, TryFromPrimitive)]
+#[repr(u8)]
+enum InterpretResult {
+    InterpretOk,
+    InterpretCompilerError,
+    InterpretRuntimeError,
+}
+
+struct VM<'a> {
+    chunk: &'a Chunk,
+    ip: usize,
+    stack: Vec<Value>,
+    stack_top: usize,
+}
+
+impl<'a> VM<'a> {
+    fn new(chunk: &'a Chunk) -> Self {
+        Self {
+            chunk,
+            ip: 0,
+            stack: Vec::with_capacity(STACK_SIZE),
+            stack_top: 0,
+        }
+    }
+
+    fn read_byte(&mut self) -> u8 {
+        let instruction = *self.chunk.code.get(self.ip).unwrap();
+        self.ip += 1;
+        return instruction;
+    }
+
+    fn read_constant(&mut self) -> Value {
+        let constant_index = self.read_byte() as usize;
+        return *self.chunk.constants.get(constant_index).unwrap();
+    }
+
+    fn reset_stack(&mut self) {
+        self.stack_top = 0;
+    }
+
+    fn push_stack(&mut self, value: Value) {
+        self.stack.push(value);
+        self.stack_top += 1;
+    }
+
+    fn pop_stack(&mut self) -> Value {
+        self.stack_top -= 1;
+        return self.stack.pop().unwrap();
+    }
+
+    fn interpret(&mut self) -> InterpretResult {
+        return self.run();
+    }
+
+    fn run(&mut self) -> InterpretResult {
+        loop {
+            if env::var("DEBUG").is_ok() {
+                print!("          ");
+                for i in 0..self.stack_top {
+                    print!("[{}]", *self.stack.get(i).unwrap())
+                }
+                println!();
+
+                disassemble_instruction(&self.chunk, self.ip);
+            }
+
+            let instruction = self.read_byte();
+            match OpCode::try_from(instruction).unwrap() {
+                OpCode::OpReturn => {
+                    let value = self.pop_stack();
+                    println!("{value}");
+                    return InterpretResult::InterpretOk;
+                }
+                OpCode::OpAdd => {
+                    binary_op!(self, +)
+                }
+                OpCode::OpSubtract => {
+                    binary_op!(self, -)
+                }
+                OpCode::OpMultiply => {
+                    binary_op!(self, *)
+                }
+                OpCode::OpDivide => {
+                    binary_op!(self, /)
+                }
+                OpCode::OpNegate => {
+                    let value = self.pop_stack();
+                    self.push_stack(-value);
+                }
+                OpCode::OpConstant => {
+                    let constant = self.read_constant();
+                    self.push_stack(constant);
+                }
+            };
+        }
     }
 }
 
@@ -70,17 +186,40 @@ fn disassemble_instruction(chunk: &Chunk, offset: usize) -> usize {
     }
 
     let instruction = *chunk.code.get(offset).unwrap();
-    match OpCode::try_from(instruction).unwrap() {
+    return match OpCode::try_from(instruction).unwrap() {
         OpCode::OpReturn => simple_instruction("OP_RETURN", offset),
+        OpCode::OpAdd => simple_instruction("OP_ADD", offset),
+        OpCode::OpSubtract => simple_instruction("OP_SUBTRACT", offset),
+        OpCode::OpMultiply => simple_instruction("OP_MULTIPLY", offset),
+        OpCode::OpDivide => simple_instruction("OP_DIVIDE", offset),
+        OpCode::OpNegate => simple_instruction("OP_NEGATE", offset),
         OpCode::OpConstant => constant_instruction("OP_CONSTANT", chunk, offset),
-    }
+    };
 }
 
 fn main() {
     let mut chunk = Chunk::new();
+
     chunk.write(OpCode::OpConstant.into(), 123);
     let constant_index = chunk.add_constant(1.2);
     chunk.write(constant_index as u8, 123);
+
+    chunk.write(OpCode::OpConstant.into(), 123);
+    let constant_index = chunk.add_constant(3.4);
+    chunk.write(constant_index as u8, 123);
+
+    chunk.write(OpCode::OpAdd.into(), 123);
+
+    chunk.write(OpCode::OpConstant.into(), 123);
+    let constant_index = chunk.add_constant(5.6);
+    chunk.write(constant_index as u8, 123);
+
+    chunk.write(OpCode::OpDivide.into(), 123);
+
+    chunk.write(OpCode::OpNegate.into(), 123);
+
     chunk.write(OpCode::OpReturn.into(), 123);
-    disassemble_chunk(&chunk, "TEST");
+
+    let mut vm = VM::new(&chunk);
+    vm.interpret();
 }
